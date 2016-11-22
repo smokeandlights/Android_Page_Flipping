5a73d6a6befe5df1513dd9dbfe7dd76adf91b72a
smokeandlights <smokeandlights@hotmail.com>
2016-11-22 10:52:04 -0500

    Add files via upload

---
+++ Android_Page_Flipping/Android_Page_Flipping.ino
@@ -0,0 +1,69 @@
+
+#include <Mouse.h>
+int pages = 3;
+int swipeLength = 9;
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Mouse.end(); //give myself a chance to upload a sketch
+  delay(20000);
+  Mouse.begin();
+  Mouse.click();
+  delay(500);
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Mouse.press();
+    Mouse.move(-swipeLength, 0, 0);
+    delay(10);
+  }
+  delay(500);
+}
+void loop() {
+  for (int i = 0; i < pages; i++) {
+    for (int i = 0; i < 10; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Mouse.press();
+      Mouse.move(-swipeLength, 0, 0);
+      delay(10);
+    }
+    delay(100);
+    Mouse.release();
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+    for (int i = 0; i < 10; i++) {
+      Mouse.move(swipeLength, 0, 0);
+      delay(10);
+    }
+  }
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Mouse.move(-swipeLength, 0, 0);
+    delay(10);
+  }
+  for (int i = 0; i < pages; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Mouse.press();
+    for (int i = 0; i < 10; i++) {
+      Mouse.move(swipeLength, 0, 0);
+      delay(10);
+    }
+    delay(100);
+    Mouse.release();
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+    for (int i = 0; i < 10; i++) {
+      Mouse.move(-swipeLength, 0, 0);
+      delay(10);
+    }
+  }
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Mouse.move(swipeLength, 0, 0);
+    delay(10);
+  }
+}
+
+
+
+
